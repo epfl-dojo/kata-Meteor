1fc66f3553466243c87eb1cd1379db01614ea1f4
Dominique Quatravaux <dominique@quatravaux.org>
2016-02-26 17:33:48 +0100

    Fix deliberate bug
    The underlying Tequila issue that I was trying to debug in this way has
    been solved (https://github.com/epfl-sti/accounts-tequila/commit/2bab0d83620ada7c0165918a04149242d2de6f32)

--- access-control.js
+++ access-control.js
@@ -1,6 +1,6 @@
 Tequila.options.request = ["displayname", "uniqueid"];
 
 Tequila.options.getUserId = function getUserId(tequilaResponse) {
-  return Meteor.users.findOne({_id: 1 + Number(tequilaResponse.uniqueid)});
+  return Meteor.users.findOne({_id: tequilaResponse.uniqueid});
 };
 
